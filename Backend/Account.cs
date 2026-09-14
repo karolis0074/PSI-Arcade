6632506934a652d8f982a63f99c61e76bfe4f1ad
karolis0074 <46167665+karolis0074@users.noreply.github.com>
2026-09-14 17:10:12 +0300

    Account klasė
    Ištrintos template klasės, sukurta Account klasė kuri laiko vartotojo duomenis ir turi metodus jų keitimui.

---
+++ Backend/Account.cs
@@ -0,0 +1,41 @@
+﻿namespace Backend
+{
+    public class Account
+    {
+        public String Username { get; private set; }
+        public int Balance { get; private set; }
+
+        private String password;
+        
+
+        public Account(String username, String password)
+        {
+            if (String.IsNullOrWhiteSpace(username) || 
+                String.IsNullOrWhiteSpace(password))
+            throw new ArgumentNullException("Account username or password is invalid.");
+
+            this.Username = username;
+            this.Balance = 5000;
+
+            this.password = password;
+            
+        }
+
+        public bool SetUsername(String username)
+        {
+            if (String.IsNullOrWhiteSpace(username))
+                return false;
+
+            this.Username = username;
+            return true;
+        }
+
+        public bool ChangePassword(String oldPassword, String newPassword)
+        {
+            if (String.IsNullOrWhiteSpace(newPassword) || !oldPassword.Equals(newPassword))
+                return false;
+            password = newPassword;
+            return true;
+        }
+    }
+}
