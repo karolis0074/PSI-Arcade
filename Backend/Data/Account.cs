99bc893f527b32d49acff78d33cb6fd11d02154c
karolis0074 <46167665+karolis0074@users.noreply.github.com>
2026-09-14 22:21:15 +0300

    Užklausos
    Paskyrų sistema prijungta prie užklausų (AccountController.cs). Užklausomis galima registruotis, prisijungti (kolkas tik grąžina rezultatą ar prisijungimo duomenys teisingi), pakeisti slaptažodį ir ištrinti paskyrą.

--- Backend/Data/Account.cs
+++ Backend/Data/Account.cs
@@ -2,13 +2,14 @@
 {
     public class Account
     {
+        public String DisplayName { get; private set; }
         public String Username { get; private set; }
         public int Balance { get; private set; }
 
-        private String password;
+        private String password; // currently stored raw, will be hashed in the future
         
 
-        public Account(String username, String password)
+        public Account(String displayName, String username, String password)
         {
             if (String.IsNullOrWhiteSpace(username) || 
                 String.IsNullOrWhiteSpace(password))
@@ -17,33 +18,34 @@
             if (Database.GetInstance().GetAccount(username) != null)
                 throw new ArgumentException("Username is taken.");
 
+            this.DisplayName = displayName;
             this.Username = username;
             this.Balance = 5000;
 
             this.password = password;
         }
 
-        public int SetUsername(String newUsername)
+        public bool SetDisplayName(String newDisplayName)
         {
-            if (String.IsNullOrWhiteSpace(newUsername))
-                return 2;  // invalid username
-            if (Database.GetInstance().GetAccount(Username) != null)
-                return 1;  // user already exists
+            if (String.IsNullOrWhiteSpace(newDisplayName))
+                return false;
 
-            Username = newUsername;
-            return 0;
+            DisplayName = newDisplayName;
+            return true;
         }
 
         public int ChangePassword(String oldPassword, String newPassword)
         {
             if (String.IsNullOrWhiteSpace(newPassword))
                 return 2; // invalid new password
-            if (!oldPassword.Equals(password))
+            if (!CheckPassword(oldPassword))
                 return 1; // incorrect current password
 
             password = newPassword;
             return 0;
         }
+
+        public bool CheckPassword(String password) => password.Equals(this.password);
 
         public bool TrySpend(int amount)
         {
---
+++ Backend/Data/Requests.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend
+{
+    public record RegisterRequest(
+        [Required] string DisplayName,
+        [Required] string Username,
+        [Required] string Password
+    );
+
+    public record LoginRequest(
+        [Required] string Username,
+        [Required] string Password
+    );
+
+    public record ChangePasswordRequest(
+        [Required] string Username,
+        [Required] string OldPassword,
+        [Required] string NewPassword
+    );
+}
