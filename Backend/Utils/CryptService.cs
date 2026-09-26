57335ffbc60b680ce3343f870a5943eb1e14270b
karolis0074 <46167665+karolis0074@users.noreply.github.com>
2026-09-26 17:02:07 +0300

    Password Hash
    Implemented password hashing:
    - Account now stores the hash instead of the raw password
    - Created CryptService which handles hashing and hash verification (will later also handle token signing, etc.)
    - Updated AccountService and AuthService so they use the CryptService when required

---
+++ Backend/Utils/CryptService.cs
@@ -0,0 +1,21 @@
+﻿using Backend;
+using Microsoft.AspNetCore.Identity;
+
+namespace SportMatch.API.Utils
+{
+    public class CryptService
+    {
+        private readonly PasswordHasher<object> _hasher = new(); // the TUser type param is not used in the default methods, so safe to pass null! below
+
+        public string Hash(string password)
+        {
+            return _hasher.HashPassword(null!, password);
+        }
+
+        public bool VerifyHash(string password, string hash)
+        {
+            var result = _hasher.VerifyHashedPassword(null!, hash, password);
+            return result == PasswordVerificationResult.Success;
+        }
+    }
+}
